6b9c2977bf9fe587532e2fec05206c127fcc5da2
Amr-El-Samra <amrsamra6161@gmail.com>
2021-05-02 00:10:12 +0200

    new

---
+++ assignment4/insertQuery - Copy.sql
@@ -0,0 +1,4 @@
+insert into stores (name , address) values ('test3','test4')
+insert into suppliers (name , phone , email , brief_data) values ('test4' , '01234','a@b.com','good')
+insert into products (name , code , price , supplier_id , description) values ('mobile','1443','100$',1,'good')
+insert into storeProducts (store_id , product_id) values (1 , 1)
